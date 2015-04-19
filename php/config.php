61277cdd26d19dc17781f0b12574f3ad64ba098f
Kevin Hoffman <keh4nb@virginia.edu>
2015-04-19 17:57:10 -0400

    Use different database user based on access level

--- php/config.php
+++ php/config.php
@@ -1,8 +1,16 @@
 <?php
-$db = new mysqli('stardock.cs.virginia.edu','cs4750klf2bf','wine','cs4750klf2bf');
+session_start();
+$db = new mysqli('stardock.cs.virginia.edu','cs4750klf2bfa','spring2015','cs4750klf2bf');
+if (isset($_SESSION["is_superuser"])) {
+
+	if ($_SESSION["is_superuser"]) {
+	$db = new mysqli('stardock.cs.virginia.edu','cs4750klf2bfb','spring2015','cs4750klf2bf');
+	}
+}
+
 if ($db->connect_errno) {
 	echo "<br> <h3>Cannot connect to database. Please try again later!</h3>";
 	exit();
 }
-session_start();
+
 ?>
